c1e27c17a66b83b2f42edab5bae7183bdae6acfa
François Risoud <francois.risoud@gmail.com>
2020-08-17 18:13:23 +0200

    remove direct Vue dependency in proxy mixin

--- web/src/mixins/proxy.js
+++ web/src/mixins/proxy.js
@@ -1,39 +1,37 @@
-import Vue from 'vue';
-
 const proxy = 'proxy__';
 const internal = 'internal__';
 
-export default (name, event) =>
-  Vue.extend({
-    name: 'state-proxy',
-    data() {
-      return {
-        [internal + name]: null,
-      };
+export default (name, event) => ({
+  name: 'state-proxy',
+  data() {
+    return {
+      [internal + name]: null,
+    };
+  },
+  created() {
+    this[internal + name] = this[name];
+  },
+  computed: {
+    [proxy + name]: {
+      get() {
+        return this[internal + name];
+      },
+      set(value) {
+        this[internal + name] = value;
+        const eventName =
+          event || (name === 'value' ? 'input' : 'update:' + name);
+        this.$emit(eventName, value);
+      },
     },
-    created() {
+  },
+  methods: {
+    proxy(_name) {
+      return this[proxy + _name];
+    },
+  },
+  watch: {
+    [name]() {
       this[internal + name] = this[name];
     },
-    computed: {
-      [proxy + name]: {
-        get() {
-          return this[internal + name];
-        },
-        set(value) {
-          this[internal + name] = value;
-          const eventName = event || (name === 'value' ? 'input' : ('update:' + name));
-          this.$emit(eventName, value);
-        },
-      },
-    },
-    methods: {
-      proxy(_name) {
-        return this[proxy + _name];
-      },
-    },
-    watch: {
-      [name]() {
-        this[internal + name] = this[name];
-      },
-    },
-  });
+  },
+});
