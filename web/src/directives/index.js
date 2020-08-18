aa95d1c4e2f2479ae5fb7b1e128ba11036f07954
François Risoud <francois.risoud@gmail.com>
2020-08-18 16:08:45 +0200

    use simple-parallax-js standalone built lib
    As npm packages installs a lot of unnecessay deps (should be dev deps, wait for our PR to reuse package)

--- web/src/directives/index.js
+++ web/src/directives/index.js
@@ -1,7 +1,7 @@
 // load client script client-side only
 let simpleParallax;
 if (process.browser) {
-  simpleParallax = require('simple-parallax-js');
+  simpleParallax = require('../assets/lib/simpleParallax.min.js');
 }
 /*
  * Directives
