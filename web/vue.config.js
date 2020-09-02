d86eda10eb337d6a25dc912d80dc7a6bad39e9f2
François Risoud <francois.risoud@gmail.com>
2020-09-02 12:43:41 +0200

    remove useless svgo-loader & config

--- web/vue.config.js
+++ web/vue.config.js
@@ -15,48 +15,5 @@ module.exports = {
       .use('svg-inline-loader')
       .loader('svg-inline-loader')
       .end()
-      .use('svgo-loader')
-      .loader('svgo-loader')
-      .options({
-        plugins: [
-          { removeTitle: true },
-          { removeViewBox: false },
-          { convertShapeToPath: true },
-          { removeAttrs: { attrs: '(fill|fill-rule)' } },
-          { inlineStyles: { onlyMatchedOnce: false } },
-          {
-            addViewBox: {
-              type: 'perItem',
-              active: true,
-              name: 'addViewBox',
-              description:
-                'replaces width, height attributes of an svg with viewBox attribute to make it responsive',
-              fn: item => {
-                if (
-                  item.isElem(['svg']) &&
-                  !item.hasAttr('viewBox') &&
-                  item.hasAttr('width') &&
-                  item.hasAttr('height')
-                ) {
-                  const width = parseFloat(
-                    item.attr('width').value.replace(/px$/, '')
-                  );
-                  const height = parseFloat(
-                    item.attr('height').value.replace(/px$/, '')
-                  );
-                  item.removeAttr('width');
-                  item.removeAttr('height');
-                  item.addAttr({
-                    name: 'viewBox',
-                    prefix: '',
-                    local: 'viewBox',
-                    value: '0 0 ' + width + ' ' + height,
-                  });
-                }
-              },
-            },
-          },
-        ],
-      });
   },
 };
