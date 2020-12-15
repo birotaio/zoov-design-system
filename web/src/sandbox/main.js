bc424740bf8a9a0a0137fdf996892923b106157e
François Risoud <francois.risoud@gmail.com>
2020-12-15 17:29:22 +0100

    add vue-router for testing purposes

--- web/src/sandbox/main.js
+++ web/src/sandbox/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue';
+import Router from 'vue-router';
 import Sandbox from './Sandbox.vue';
+
+Vue.use(Router);
+
+// the router instance for testing
+const router = new Router();
 
 import zds from '@';
 import * as components from '@/components';
@@ -8,4 +14,4 @@ Vue.use(zds, { components });
 
 Vue.config.productionTip = false;
 
-new Vue({ render: h => h(Sandbox) }).$mount('#app');
+new Vue({ router, render: h => h(Sandbox) }).$mount('#app');
