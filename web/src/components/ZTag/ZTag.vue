6c3d07f78307d53bb570207649d90757161c606b
François Risoud <francois.risoud@gmail.com>
2020-12-29 12:59:01 +0100

    handle color (colored text & fixed transparent background)

--- web/src/components/ZTag/ZTag.vue
+++ web/src/components/ZTag/ZTag.vue
@@ -1,33 +1,47 @@
-  .z-tag.px-1(:style="{ 'background': background, 'color': color }")
 <template lang="pug" functional>
 .z-tag(
   :ref="data.ref"
+  :class="[data.class, data.staticClass, props.color ? 'text--' + props.color : '']"
   :style="[data.style, data.staticStyle]"
   v-on="listeners"
 )
+  .z-tag__background(:class="[props.color, props.color ? 'z-tag__background--colored' : '']")
   small.z-tag__content
     slot
 </template>
 
 <style lang="stylus">
-@import '~@zoov/design-system/src/styles/components';
+@import '../../styles/components.styl';
+
 .z-tag
-  // display: inline-block
+  position relative
+  display inline-block
+  padding 0 size(1)
   max-width max-content
+  text-transform uppercase
+  color: $colors.neutral.dark-4
+
+.z-tag__background
+  absolute-full()
+  background-color white
   border-radius size(2.5)
+  z-index 0
+
+  &--colored
+    opacity 0.1
+
+.z-tag__content
+  position relative
+  z-index 1
 </style>
 
 <script>
 export default {
   name: 'Ztag',
   props: {
-    background: {
-      type: String,
-      default: 'white',
-    },
     color: {
       type: String,
-      default: 'black',
+      default: '',
     },
   },
 };
